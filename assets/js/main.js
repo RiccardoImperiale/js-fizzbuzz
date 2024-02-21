57d44045cc909054e8e7657232f34ed5fc9fd881
RiccardoImperiale <riccardoimperiale.dev@gmail.com>
2024-02-21 14:57:33 +0100

    add squares into page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,13 @@
+const gameBoard = document.querySelector('.game_board')
 
 // generate numbers from 1 to 100
 for (let i = 1; i <= 100; i++) {
+    // generate squares
+    const squareMarkup = `
+    <div class="number d-flex align-items-center justify-content-center bg-white">
+        ${i}
+    </div>`
+    gameBoard.insertAdjacentHTML('beforeend', squareMarkup)
     // console.log(i);
     if (i % 3 === 0 && i % 5 === 0) {
         // numbers multiple of 3 and 5
