435e81501fee6a19e61971eacb9e5c56a3315496
RiccardoImperiale <riccardoimperiale.dev@gmail.com>
2024-02-21 14:11:18 +0100

    add check for numbers

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,15 @@
 
 // generate numbers from 1 to 100
 for (let i = 1; i <= 100; i++) {
-    console.log(i);
-    
+    // console.log(i);
+    if (i % 3 === 0 && i % 5 === 0) {
+        // numbers multiple of 3 and 5
+        console.log(i, 'fizzbuzz');
+    } else if (i % 3 === 0) {
+        // numbers multiple of 3
+        console.log(i, 'fizz');
+    } else if (i % 5 === 0) {
+        // numbers multiple of 5
+        console.log(i, 'buzz');
+    }
 }
