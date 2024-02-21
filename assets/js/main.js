de0c5b65570dc0026397f038f2ee88734e5e6ed1
RiccardoImperiale <riccardoimperiale.dev@gmail.com>
2024-02-21 15:47:57 +0100

    add css style

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,12 @@
 const gameBoard = document.querySelector('.game_board')
-// const square = document.querySelector('.number')
-// const squareMarkup = `
-// <div class="number d-flex align-items-center justify-content-center bg-white">
-//     ${i}
-// </div>`
 
 // generate numbers from 1 to 100
 for (let i = 1; i <= 100; i++) {
     // generate square
     const square = document.createElement('div')
-    // add classes square
-    square.className = 'number d-flex align-items-center justify-content-center'
+    // add classes to square
+    square.className = 'number rounded d-flex align-items-center justify-content-center'
 
-    // console.log(i);
     if (i % 3 === 0 && i % 5 === 0) {
         console.log(i, 'fizzbuzz');
         square.innerText = 'FizzBuzz'
